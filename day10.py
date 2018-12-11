5284f2e2425f55accc2376046f665fbefb5f42e0
Elgolfin <elgo@nicomputer.net>
2018-12-10 19:28:50 -0500

    Fix the way the Day 10 solutions are displayed

--- day10.py
+++ day10.py
@@ -6,6 +6,7 @@ import day10_lib
 with open(sys.argv[1], 'r') as inputFile:
     INPUT = inputFile.readlines()
 
+
+print("Day10 --- Part One --- result is: ")
 DURATION = day10_lib.getMessage(INPUT)
-#print("Day10 --- Part One --- result is: {0}".format(DURATION))
-print("Day07 --- Part Two --- result is: {0}".format(DURATION))
+print("Day10 --- Part Two --- result is: {0}".format(DURATION))
