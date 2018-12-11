fc29db932f5d52bb53950b0c4f2f80f15d1f0496
Elgolfin <elgo@nicomputer.net>
2018-12-10 19:22:17 -0500

    Solve Day 10 Part 2 puzzle

--- day10.py
+++ day10.py
@@ -7,5 +7,5 @@ with open(sys.argv[1], 'r') as inputFile:
     INPUT = inputFile.readlines()
 
 DURATION = day10_lib.getMessage(INPUT)
-print("Day10 --- Part One --- result is: {0}".format(DURATION))
-# print("Day07 --- Part Two --- result is: {0}".format(day07_lib.completeSteps(STARTING_STEPS, STEPS, 5, 60)))
+#print("Day10 --- Part One --- result is: {0}".format(DURATION))
+print("Day07 --- Part Two --- result is: {0}".format(DURATION))
