4138608e990906b670ec3778ecdce4ff78c3b0f0
Elgolfin <elgo@nicomputer.net>
2018-12-19 20:53:37 -0500

    Get a first attempt to solve the Day 17 Part 1 puzzle

--- day17.py
+++ day17.py
@@ -6,6 +6,6 @@ import day17_lib
 with open(sys.argv[1], 'r') as inputFile:
     INPUT = inputFile.readlines()
 
-day17_lib.initializeGround(INPUT)
-# print("Day12 --- Part One --- result is: {0}".format(RESULT))
+GROUND = day17_lib.initializeGround(INPUT)
+print("Day17 --- Part One --- result is: {0}".format(day17_lib.countWaterTiles(GROUND, (500, 0))))
 # print("Day12 --- Part Two --- result is: {0}".format(RESULT))
