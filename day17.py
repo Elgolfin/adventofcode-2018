688ecb66d6415cd079863e59e072fae8536569e7
Elgolfin <elgo@nicomputer.net>
2018-12-20 19:56:06 -0500

    Create the Day 17 bootstrap solution

--- day17.py
+++ day17.py
@@ -7,5 +7,6 @@ with open(sys.argv[1], 'r') as inputFile:
     INPUT = inputFile.readlines()
 
 GROUND = day17_lib.initializeGround(INPUT)
-print("Day17 --- Part One --- result is: {0}".format(day17_lib.countWaterTiles(GROUND, (500, 0))))
-# print("Day12 --- Part Two --- result is: {0}".format(RESULT))
+WATER_TILES, STILL_WATER_TILES = day17_lib.countWaterTiles(GROUND, (500, 0), False)
+print("Day17 --- Part One --- result is: {0}".format(WATER_TILES))
+print("Day17 --- Part Two --- result is: {0}".format(STILL_WATER_TILES))
